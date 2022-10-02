a665588414e49207afc5e07ac0ad2cf6b25ffc82
chenjianfeng <chenjianfeng93@163.com>
2022-10-02 23:06:51 +0800

    feat: add allPossibleFullBinaryTrees's case

---
+++ BST/allPossibleFullBinaryTrees.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: chenjianfeng chenjianfeng93@163.com
+ * @Date: 2022-10-02 23:04:14
+ * @Description: 
+ */
+// https://leetcode.cn/problems/all-possible-full-binary-trees/description/
+
+// 输入：n = 7
+// 输出：[[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
+
+
+const count = 7
+
+const allPossibleFBT = (n) => {}
